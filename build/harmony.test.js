3ffd03d8f01fb9204955443224c83aa88104d312
GermanBluefox <dogafox@gmail.com>
2025-11-03 11:34:16 +0100

    Fixing tests

---
+++ build/harmony.test.js
@@ -0,0 +1,23 @@
+/**
+ * This is a dummy TypeScript test file using chai and mocha
+ *
+ * It's automatically excluded from npm, and its build output is excluded from both git and npm.
+ * It is advised to test all your modules with accompanying *.test.ts-files
+ */
+import 'chai/register-should';
+import { expect } from 'chai';
+// import { functionToTest } from "./moduleToTest";
+describe('module to test => function to test', () => {
+    // initializing logic
+    const expected = 5;
+    it(`should return ${expected}`, () => {
+        const result = 5;
+        // assign result a value from functionToTest
+        expect(result).to.equal(expected);
+        // or using the should() syntax
+        result.should.equal(expected);
+    });
+    // ... more tests => it
+});
+// ... more test suites => describe
+//# sourceMappingURL=harmony.test.js.map
