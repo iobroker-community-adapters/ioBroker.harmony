cb88fefd41ace8bde7aa73d0ce957e9b95d63e18
Martin M. <mcm57@gmx.at>
2025-11-03 14:12:07 +0100

    remove unused chai/register-should

--- src/harmony.test.ts
+++ src/harmony.test.ts
@@ -1,11 +1,10 @@
 /**
  * This is a dummy TypeScript test file using chai and mocha
  *
- * It's automatically excluded from npm, and its build output is excluded from both git and npm.
+ * It's automatically excluded from npm and its build output is excluded from both git and npm.
  * It is advised to test all your modules with accompanying *.test.ts-files
  */
 
-/*import 'chai/register-should';
 import { expect } from 'chai';
 // import { functionToTest } from "./moduleToTest";
 
@@ -24,4 +23,3 @@ describe('module to test => function to test', () => {
 });
 
 // ... more test suites => describe
-*/
